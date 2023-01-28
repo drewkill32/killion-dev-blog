b116a349f27373e6d759154ce7bc3a64c1e25f5d
Drew Killion <adk@killion.me>
2023-01-28 11:15:04 -0500

    upgrading astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import { addExcerpt } from "./addExcerpt";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    extendDefaultPlugins: true,
-    remarkPlugins: [addExcerpt]
-  },
-  integrations: [image(), mdx()]
+  integrations: [
+    mdx({
+      remarkPlugins: [addExcerpt],
+    }),
+  ],
 });
